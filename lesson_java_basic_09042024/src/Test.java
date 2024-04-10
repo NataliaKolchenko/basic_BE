80f8237b97dc2958b083c1d302eeb9b6aa9604fe
Natalia Kolchenko <160338391+NataliaKolchenko@users.noreply.github.com>
2024-04-10 09:57:31 +0200

    lesson_java_basic_09042024
    lesson_java_basic_09042024

---
+++ lesson_java_basic_09042024/src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+    public static void main (String [] args) {
+
+        int b = 44;
+        //int i = 455;
+        //double doo = 55.7;
+
+        //System.out.println(b+i);
+        //b = b+100;
+        byte b1 = 100;
+        byte res = (byte) (b+b1);
+        System.out.println(res);
+
+
+
+
+    }
+}
