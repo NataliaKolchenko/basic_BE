7ed41c0a1114ff5868c1900b0f44c1b9677557f3
Natalia <kolchenko.nata.yu@gmail.com>
2024-04-11 14:08:34 +0200

    Full homework 1

---
+++ HomeWork_1/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+// Task
+//        В методе main добавить инструкцию System.out.println(); необходимое количество раз, чтобы в консоль распечатался текст:
+//                Hello,
+//                my
+//                name
+//                is
+//                !
+//        Используя инструкцию System.out.print(); распечатайте в консоль текст:
+//        Hello, my name is !
+
+
+
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("!");
+
+        System.out.print("Hello, my name is !");
+
+        System.out.println(" ");
+
+
+        String str = "Hello, my name is !";
+        System.out.println(str);
+
+
+    }
+}
