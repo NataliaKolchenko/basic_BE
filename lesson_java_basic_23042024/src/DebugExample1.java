b68bb5602f3cb08ca5c107336c11a6646838cc43
Natalia <kolchenko.nata.yu@gmail.com>
2024-04-23 12:38:33 +0200

    lesson 23042024: debug, extra exercise

---
+++ lesson_java_basic_23042024/src/DebugExample1.java
@@ -0,0 +1,13 @@
+public class DebugExample1 {
+    public static void findEvenOdd(int num) {
+        if (num%2 == 0) {
+            System.out.println(num+" is even"); }
+        else {
+            System.out.println(num+" is odd"); }
+    }
+    public static void main (String[] args) {
+        findEvenOdd(2);
+        findEvenOdd(4);
+        findEvenOdd(5);
+    }
+}
