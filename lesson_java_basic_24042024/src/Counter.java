bac7334c6762ed4eedb25c4d3dd80e0b0cc2ecf3
Natalia <kolchenko.nata.yu@gmail.com>
2024-04-25 22:02:34 +0200

    lesson 24042024: logical expressions, unary expressions, extra tasks

--- lesson_java_basic_24042024/src/Counter.java
+++ lesson_java_basic_24042024/src/Counter.java
@@ -40,7 +40,7 @@ public class Counter {
 //    }
 
     public static void main(String[] args) {
-        for (int i = 1; i < 20; i++) {
+        for (int i = 2; i < 20; i++) {
             if(i > 10) {
                 System.out.println(20 - i);
             } else {
