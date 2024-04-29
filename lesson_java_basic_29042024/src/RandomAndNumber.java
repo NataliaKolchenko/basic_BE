c7884c7eb3947bd667b7173f1ac422f0161d4d7d
Natalia <kolchenko.nata.yu@gmail.com>
2024-04-29 22:26:31 +0200

    lesson 26042024: if-else, enum, switch

--- lesson_java_basic_29042024/src/RandomAndNumber.java
+++ lesson_java_basic_29042024/src/RandomAndNumber.java
@@ -8,13 +8,57 @@ import java.util.Random;
 public class RandomAndNumber {
 
     public static void main(String[] args) {
-        Random random = new Random();
+//        Random random = new Random();
         int min = 100;
         int max = 999;
 
          int randomNumber = (int)(Math.random()*(max - min + 1)+min);
+//         int randomNumber = 812;
 
-         int singleNumber = 0;
+        System.out.println("Random number is " + randomNumber);
+
+        int numberThree = randomNumber % 10; //третье число
+        int number2 = randomNumber / 10;
+        int numberTwo = number2 % 10; //второе число
+        int numberOne = number2 / 10; //первое число
+//        System.out.println(number1);
+//        System.out.println(number3);
+//        System.out.println(number4);
+
+        int largerNumber = 0;
+
+//        if (numberOne >= numberTwo) {
+//            largerNumber = numberOne;
+//        } else {
+//            largerNumber = numberTwo;
+//        }
+//
+//        if (numberOne >= numberThree) {
+//            largerNumber = numberOne;
+//        } else {
+//            largerNumber = numberThree;
+//        }
+//
+//        if (numberTwo >= largerNumber) {
+//            largerNumber = numberTwo;
+//        } else {
+//            largerNumber = largerNumber;
+//        }
+
+        largerNumber = numberOne >= numberTwo
+                ? numberOne
+                : numberTwo;
+
+        largerNumber = numberOne >= numberThree ? numberOne : numberThree;
+
+        largerNumber = numberTwo >= largerNumber
+                ? numberTwo
+                : largerNumber;
+
+
+        System.out.println("The largest number is " + largerNumber);
+
+
 
 
     }
