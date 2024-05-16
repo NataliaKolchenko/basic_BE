b890b83f6335e7743f604d3593636a2287decec6
Natalia <kolchenko.nata.yu@gmail.com>
2024-05-16 21:00:13 +0200

    lesson_java_basic_09042024

--- lesson_java_basic_04_09042024/src/Main.java
+++ lesson_java_basic_04_09042024/src/Main.java
@@ -1,3 +1,5 @@
+package lesson_java_basic_04_09042024.src;
+
 public class Main {
     public static void main(String[] args) {
 
--- lesson_java_basic_04_09042024/src/Test.java
+++ lesson_java_basic_04_09042024/src/Test.java
@@ -1,3 +1,5 @@
+package lesson_java_basic_04_09042024.src;
+
 public class Test {
 
     public static void main (String [] args) {
